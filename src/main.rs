dd7b96034c080d48c5aff926a8c614ab93acacdf
Olimdzhon <write2sd@mail.ru>
2024-07-19 12:58:37 +0500

    Strings; Loop Through Strings;

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 // mod print;
 // mod vars;
-mod types;
+// mod types;
+mod strings;
 
 fn main() {
     // print::run()
     // vars::run()
-    types::run()
+    // types::run()
+    strings::run()
 }
